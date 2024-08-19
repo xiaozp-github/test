c5fe15398ce8c58306dff8a15bffc5eb4b90d2d0
xiaozp <379127976@qq.com>
2024-08-19 10:13:28 +0800

    测试提交代码GitHub

---
+++ test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	//printf("hehe\n");
+	int a = 0;
+	scanf("%d", &a);
+	printf("%d\n", a);
+
+	return 0;
+}
+
+
+//newc++file.cpp
+// 
+// 
+//strcpy
+//strcat
+//scanf
